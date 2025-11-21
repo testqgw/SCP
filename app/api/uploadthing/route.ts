591dd3c3f988ba6f593e9be709a326789e7d8843
Quinc <quinc@example.com>
2025-11-20 19:15:42 -0500

    Downgrade to UploadThing v6 for stability

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,3 +1,6 @@
-import { createRouteHandler } from "uploadthing/next";
+import { createNextRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
-export const { GET, POST } = createRouteHandler({ router: ourFileRouter });
+
+export const { GET, POST } = createNextRouteHandler({
+    router: ourFileRouter,
+});
