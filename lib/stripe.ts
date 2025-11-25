bfd1190120f7031f6890172e98131e0494f2e455
Quinc <quinc@example.com>
2025-11-25 13:59:28 -0500

    Fix: Update Stripe API version to match installed package

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-    apiVersion: "2024-11-20.acacia", // Using latest stable version
+    apiVersion: "2025-02-24.acacia", // Using latest stable version
     typescript: true,
 });
